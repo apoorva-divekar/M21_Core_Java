6e83d6696cf910bf109b5160bc00129b7943b57e
admin <admin@DESKTOP-0B3GOM4>
2022-08-14 22:37:58 +0530

    First Commit

---
+++ Day19_JDBC/src/com/cg/jdbc/DBconnect.java
@@ -0,0 +1,13 @@
+package com.cg.jdbc;
+
+public class DBconnect {
+	public static void main(String[]args) {
+		String dbDriver="com.mysql.cg.jdbc.Driver";
+		//to connect eclipse with mysql server
+		String dbURL="jdbc:mysql://localhost:3306/capgemini";
+		String dbUsername="root";
+		String dbPassword="Apoorva@26";
+		String query=""
+	}
+
+}
